32e50e6e04207e2bd8930541b9b0733fb7a6eb58
Boyd Lever <boydlever@stanfordhacker.com>
2023-02-01 10:40:53 -0800

    02.01.23 10:40AM

--- client/src/components/NewPost.js
+++ client/src/components/NewPost.js
@@ -8,6 +8,8 @@ import { Input } from '@mui/material';
 import { useState } from 'react';
 import axios from 'axios';
 import { Navigate, useNavigate } from 'react-router-dom';
+import FileBase64 from 'react-file-base64';
+import base64Style from './main.module.css';
 
 const inputSX = {
     border: 1,
@@ -28,7 +30,7 @@ const buttonSX = {
 }
 
 const NewPost = (props) => {
-  
+
     const [formData, setFormData] = useState({
         content: "",
         flavor: "",
@@ -37,6 +39,7 @@ const NewPost = (props) => {
 
     const [errors, setErrors] = useState({});
     const navigate = useNavigate();
+    const [img, setImg] = useState({});
 
     const onChangeHandler = e => {
         setFormData({
@@ -58,74 +61,81 @@ const NewPost = (props) => {
             .catch(err => {
                 // console.log("test");
                 // navigate('/error');
-                setErrors({errors: 'Email or password is incorrect'});
+                setErrors({ errors: 'Email or password is incorrect' });
             })
     }
-    
-  return (
-    <Box>
-        {/* IMG PREVIEW GOES HERE */}
+
+    return (
         <Box>
-        <Box
-            sx={{
-                width: '50%',
-                mx: 'auto', // margin left & right
-                my: 4, // margin top & botom
-                py: 3, // padding top & bottom
-                px: 2, // padding left & right
-                display: 'flex',
-                flexDirection: 'column',
-                gap: 2,
-                border: 1,
-                borderColor: '#F92F60',
-                borderRadius: '50px',
-                boxShadow: 'md'
-            }}
-        >
-            <Box sx={{
-                display: 'flex',
-                alignItems: 'center'
-            }}>
-            
-            <Typography sx={{ color: '#CA0B4A' }} variant="h3">Cook Up A Post</Typography>
-            </Box>
-                {
-                    errors.errors && (
-                        <Typography sx={{color: 'red', mx: 'auto'}}> {errors.errors} </Typography>
-                    )
-                }
-            <FormControl>
-                <FormLabel sx={{ color: '#CA0B4A' }}>Content</FormLabel>
-                <Input
-                    sx={inputSX}
-                    // html input attribute
-                    onChange={onChangeHandler}
-                    name="content"
-                    type="content"
-                    placeholder="A selfie with my kitten"
-                />
-            </FormControl>
-            <FormControl>
-                <FormLabel sx={{ color: '#CA0B4A' }}>Flavors</FormLabel>
-                <Input
+            {/* IMG PREVIEW GOES HERE */}
+            <Box>
+                <Box
                     sx={{
+                        width: '50%',
+                        mx: 'auto', // margin left & right
+                        my: 4, // margin top & botom
+                        py: 3, // padding top & bottom
+                        px: 2, // padding left & right
+                        display: 'flex',
+                        flexDirection: 'column',
+                        gap: 2,
                         border: 1,
-                        borderColor: '#f92f60',
-                        color: '#CA0B4A'
+                        borderColor: '#F92F60',
+                        borderRadius: '50px',
+                        boxShadow: 'md'
                     }}
-                    onChange={onChangeHandler}
-                    name="flavor"
-                    type="flavor"
-                    placeholder="Selfie"
-                />
-            </FormControl>
-            <Button sx={buttonSX} onClick={onSubmitHandler}>
-                POST
-            </Button>
+                >
+                    <Box sx={{
+                        display: 'flex',
+                        alignItems: 'center'
+                    }}>
+
+                        <Typography sx={{ color: '#CA0B4A' }} variant="h3">Cook Up A Post</Typography>
+                    </Box>
+                    {
+                        errors.errors && (
+                            <Typography sx={{ color: 'red', mx: 'auto' }}> {errors.errors} </Typography>
+                        )
+                    }
+                    <FormControl>
+                        <div className = {base64Style.inputfile} >
+                            <FileBase64
+                                type='file'
+                                multiple={false}
+                                onDone={({ base64 }) => setImg({ img: base64 })} />
+                        </div>
+
+                        <FormLabel sx={{ color: '#CA0B4A' }}>Content</FormLabel>
+                        <Input
+                            sx={inputSX}
+                            // html input attribute
+                            onChange={onChangeHandler}
+                            name="content"
+                            type="content"
+                            placeholder="A selfie with my kitten"
+                        />
+                    </FormControl>
+                    <FormControl>
+                        <FormLabel sx={{ color: '#CA0B4A' }}>Flavors</FormLabel>
+                        <Input
+                            sx={{
+                                border: 1,
+                                borderColor: '#f92f60',
+                                color: '#CA0B4A'
+                            }}
+                            onChange={onChangeHandler}
+                            name="flavor"
+                            type="flavor"
+                            placeholder="Selfie"
+                        />
+                    </FormControl>
+                    <Button sx={buttonSX} onClick={onSubmitHandler}>
+                        POST
+                    </Button>
+                </Box>
+            </Box>
         </Box>
-        </Box>
-    </Box>
-  )
+    )
 }
 
 export default NewPost
