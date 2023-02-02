fe3daed910064a2b56fb16e3380abd9fea1397f4
Boyd Lever <boydlever@stanfordhacker.com>
2023-02-02 15:50:29 -0800

    02.02.23 3:50PM

--- server/routes/User.routes.js
+++ server/routes/User.routes.js
@@ -7,13 +7,6 @@ module.exports = function(app){
     app.get('/api/getByEmail/:email', Users.getByEmail);
     app.get('/api/getById/:id', Users.getById);
     app.put('/api/updateUser/:email', Users.update);
-    app.get("/api/users", Users.getAll); //Has authentications
+    app.get("/api/users", Users.getAll);
     app.delete("/api/user/:id", Users.delete);
 }
-
-// // inside of user.routes.js
-// const Users = require('../controllers/user.controller');
-// module.exports = app => {
-//   // this route now has to be authenticated
-// }
-
